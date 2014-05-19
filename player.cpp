bc6128c3873c4b1747bc50d17a7edf7a0f5159a8
Ross Simpson <ross@onedigitalmedia.com>
2014-05-19 09:08:12 +0200

    Add license header to all files

--- player.cpp
+++ player.cpp
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2014 Ross Simpson
+ * All Right Reserved
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * To view a copy of this license,visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+ */
+
 #include "player.h"
 #include "gamestate.h"
 
